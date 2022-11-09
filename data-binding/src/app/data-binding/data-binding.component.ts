fa2cfa11efc057a9db5945c4854ee1c5103232b1
Emanuel Novais <105754617+30emanuel@users.noreply.github.com>
2022-11-09 12:05:17 -0300

    Data Binding 1/2
    Property Binding e Interporlação
    Class e Style Binding
    Event  Binding
    Two-way data binding

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://loiane.com'
+  cursoAngular: boolean = true
+  urlImagem: string = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Cruzeiro_Esporte_Clube_%28logo%29.svg/1200px-Cruzeiro_Esporte_Clube_%28logo%29.svg.png'
+  valorAtual: string = ''
+  valorSalvo: string = ''
+  isMouseOver: boolean = false
+
+  botaoClicado(){
+    alert('Botão clicado!')
+  }
+
+  getValor(){
+    return 1
+  }
+
+  getCurtirCurso(){
+    return true
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value
+  }
+
+  salvarValor(valor: string){
+    this.valorSalvo = valor
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
