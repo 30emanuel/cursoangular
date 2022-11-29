a407bd02bd7147cf6493681e5d0b755bf989ae07
Emanuel Novais <105754617+30emanuel@users.noreply.github.com>
2022-11-29 14:33:42 -0300

    refaturando

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { HomeComponent } from './home/home.component';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+//import { CursosComponent } from './cursos/cursos.component';
+//import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+//import { CursosService } from './cursos/cursos.service';
+//import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+
+import { CursosModule } from './cursos/cursos.module';
 import { AppRoutingModule } from './app.routing.module';
 
 
@@ -17,19 +19,17 @@ import { AppRoutingModule } from './app.routing.module';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    CursosComponent,
     HomeComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     //AppRoutingModule,
     BrowserAnimationsModule,
+    CursosModule,
     AppRoutingModule
   ],
-  providers: [CursosService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -1,29 +1,18 @@
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from '@angular/router';
 
-import { Router, RouterModule, Routes } from '@angular/router';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
-
-import { CursosComponent } from './cursos/cursos.component';
+//import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+//import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+//import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
-  {
-    path: '', component: HomeComponent
-  },
-  {
-    path: 'login', component: LoginComponent
-  },
-  {
-    path: 'cursos', component: CursosComponent
-  },
-  {
-    path: 'curso/:id', component: CursoDetalheComponent
-  },
-  {
-    path: 'naoEncontrado', component: CursoNaoEncontradoComponent
-  }
+  //{path: 'cursos', component: CursosComponent},
+  //{path: 'curso/:id', component: CursoDetalheComponent},
+  //{path: 'naoEncontrado', component: CursoNaoEncontradoComponent}
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
 ];
 
 @NgModule({
