11a16b56f1943b123217cafbec120ac376805e54
Emanuel Novais <105754617+30emanuel@users.noreply.github.com>
2022-11-30 15:19:51 -0300

    Update alunos.service.ts

--- rotas/src/app/alunos/alunos.service.ts
+++ rotas/src/app/alunos/alunos.service.ts
@@ -8,7 +8,7 @@ export class AlunosService {
   private alunos: any[] = [
     {id: 1, nome: 'Alunos 01', email: 'aluno01@gmail.com'},
     {id: 2, nome: 'Alunos 02', email: 'aluno02@gmail.com'},
-    {id: 1, nome: 'Alunos 03', email: 'aluno03@gmail.com'}
+    {id: 3, nome: 'Alunos 03', email: 'aluno03@gmail.com'}
   ]
 
   getAlunos(){
