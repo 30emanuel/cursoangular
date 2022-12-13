06a12f99e903979c274cb9460be0724f4491d5af
Emanuel Novais <105754617+30emanuel@users.noreply.github.com>
2022-12-13 11:08:39 -0300

    autocomplete com viacep

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template-form',
@@ -14,16 +16,57 @@ export class TemplateFormComponent implements OnInit {
 
   onSubmit(form: any){
     console.log(form.value)
-    console.log(this.usuario)
+    
+    this.http.post('https://httpbin.org/post', JSON.stringify(form.value)).pipe(map(res => res)).subscribe(dados => console.log(dados))
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   verificaValidTouched(campo:any){
     return !campo.valid && campo.touched
+  }
+
+  consultaCep(cep: string, form: any){
+    cep = cep.replace(/\D/g, '')
+    if(cep != ''){
+      var validacep = /^[0-9]{8}$/
+
+      this.resetaDadosForm(form)
+
+      if(validacep.test(cep)){
+        this.http.get(`//viacep.com.br/ws/${cep}/json/`)
+          .pipe(map((dados: any) => dados))
+          .subscribe(dados => this.populaDadosForm(dados, form))
+      }
+    }
+  }
+
+  populaDadosForm(dados: any, formulario: any){
+    formulario.form.patchValue({
+      endereco:{
+        rua: dados.logradouro,
+        cep: dados.cep,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    })
+  }
+
+  resetaDadosForm(formulario:any){
+    formulario.form.patchValue({
+      endereco:{
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    })
   }
 
 }
