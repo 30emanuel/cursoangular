a476bc04301e67f31d6eac325317b6c406ccdb90
Emanuel Novais <105754617+30emanuel@users.noreply.github.com>
2022-12-22 13:17:02 -0300

    pipe async, variavel ambiente, lista e rxjs unsubscribe

---
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Curso } from '../curso';
+import { CursosService } from '../cursos.service';
+
+@Component({
+  selector: 'app-cursos-lista',
+  templateUrl: './cursos-lista.component.html',
+  styleUrls: ['./cursos-lista.component.css'],
+  preserveWhitespaces: true
+})
+export class CursosListaComponent implements OnInit {
+
+  //cursos: Curso[] = []
+
+  cursos$!: Observable<Curso[]>
+
+  constructor(private service: CursosService) { }
+
+  ngOnInit(): void {
+    //this.service.list()
+    //.subscribe(dados => this.cursos = dados)
+
+    this.cursos$ = this.service.list()
+
+  }
+
+
+
+}
