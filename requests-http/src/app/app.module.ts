124b8c7437ccbe7f2275a3dc7cbbbc41285e60dd
Emanuel Novais <105754617+30emanuel@users.noreply.github.com>
2022-12-23 12:57:54 -0300

    serviço de alerta

--- requests-http/src/app/app.module.ts
+++ requests-http/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsModalService, ModalModule } from 'ngx-bootstrap/modal'
 
 @NgModule({
   declarations: [
@@ -12,9 +12,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ModalModule.forRoot(),
   ],
-  providers: [],
+  providers: [BsModalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, empty, Observable, Subject } from 'rxjs';
 import { Curso } from '../curso';
 import { CursosService } from '../cursos.service';
+import { AlertModalService } from 'src/app/shared/alert-modal.service';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -11,20 +12,33 @@ import { CursosService } from '../cursos.service';
 })
 export class CursosListaComponent implements OnInit {
 
-  //cursos: Curso[] = []
-
   cursos$!: Observable<Curso[]>
+  error$ = new Subject<boolean>()
 
-  constructor(private service: CursosService) { }
+  constructor(
+    private service: CursosService,
+    private alertService: AlertModalService) { }
 
   ngOnInit(): void {
     //this.service.list()
     //.subscribe(dados => this.cursos = dados)
-
-    this.cursos$ = this.service.list()
-
+    this.onRefresh()
   }
 
+  onRefresh(){
+    this.cursos$ = this.service.list()
+    .pipe(
+      catchError(error => {
+        console.log(error)
+        //this.error$.next(true)
+        this.handleError()
+        return empty()
+      })
+    )
+  }
 
+  handleError(){
+    this.alertService.showAlertDanger('Erro ao carregar cursos. Tente novamente mais tarde.')
+  }
 
 }
