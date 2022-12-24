38eb630064e60721894a98b0aebfbd0bfeef33a8
Emanuel Novais <105754617+30emanuel@users.noreply.github.com>
2022-12-24 10:05:46 -0300

    criando formulário para criar e editar

--- requests-http/src/app/app.module.ts
+++ requests-http/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BsModalService, ModalModule } from 'ngx-bootstrap/modal'
+import { BsModalService, ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
